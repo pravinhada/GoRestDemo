d95dc93819046bf3bfdcbfe7b96c1311831bbf3e
pravin <pravin_hada@yahoo.com>
2019-11-29 21:19:44 -0500

    adding more test case

--- api_test.go
+++ api_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -23,5 +24,24 @@ func TestDefaultEvents(t *testing.T) {
 	expectedValue := "Docker in Action"
 	if !strings.Contains(rr.Body.String(), expectedValue) {
 		t.Errorf("The expected value [%v] is not in the default json", expectedValue)
+	}
+}
+
+func TestGetOneEvent(t *testing.T)  {
+	req, err := http.NewRequest("GET", "/event/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/event/{id}", getOneEvent)
+	router.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expectedValue := "Introduction to Golang"
+	if !strings.Contains(rr.Body.String(), expectedValue) {
+		t.Errorf("The expected value [%v] is not in the result json", expectedValue)
 	}
 }
